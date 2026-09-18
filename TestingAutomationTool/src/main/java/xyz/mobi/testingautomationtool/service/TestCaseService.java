330bc19594eee0490bcd0b2cab6108d99cf94984
Mithuna Arjunan <mithuna.a@mobiasia.com>
2026-09-18 17:57:33 +0530

    Implement test case GET APIs

--- TestingAutomationTool/src/main/java/xyz/mobi/testingautomationtool/service/TestCaseService.java
+++ TestingAutomationTool/src/main/java/xyz/mobi/testingautomationtool/service/TestCaseService.java
@@ -1,4 +1,14 @@
 package xyz.mobi.testingautomationtool.service;
 
+import xyz.mobi.testingautomationtool.dto.TestCaseExecutionDTO.TestCaseExecutionRequest;
+import xyz.mobi.testingautomationtool.dto.TestCaseExecutionDTO.TestCaseExecutionResponse;
+
 public interface TestCaseService {
+
+    TestCaseExecutionResponse createTestCaseByManual(
+            TestCaseExecutionRequest request);
+
+    TestCaseExecutionResponse createTestCaseByUpload(
+            TestCaseExecutionRequest request);
+
 }
